de44daa14d6701f03b0e568635160eb9cd79f299
Shamus Clifford <shamusclifford@bpsma.org>
2019-07-07 15:01:10 -0400

    Ownable does not inherit from SupplyChain.sol

--- project-6/contracts/coffeecore/Ownable.sol
+++ project-6/contracts/coffeecore/Ownable.sol
@@ -1,9 +1,7 @@
 pragma solidity ^0.5.7;
 
-import '../coffeebase/SupplyChain.sol';
-
 /// Provides basic authorization control
-contract Ownable is SupplyChain {
+contract Ownable {
     address private origOwner;
 
     // Define an Event
